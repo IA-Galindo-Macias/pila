f8177ad94828a4ad9bd36aa269b40471a53ab7f7
E. Galindo <78140218+Galindo-lab@users.noreply.github.com>
2024-04-05 12:46:36 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -19,5 +19,48 @@ class Pila():
     def size(self):
         return len(self.pila)
 
-pila = Pila()
-pila.push(56)
+
+class TestPila(unittest.TestCase):
+
+    def test_push_when_empty(self):
+        test_value = 0
+        pila = Pila() # pila vacia
+        pila.push(test_value)
+
+        self.assertEqual(pila.size(), 1, "Pila tiene un elemento")
+        self.assertIn(test_value, pila.pila, "El valor esta en la pila")
+
+    def test_push_when_not_empty(self):
+        
+        # pila no vacia
+        pila = Pila() 
+        pila.pila = [1,2,3,4]
+
+        # agregar valor 
+        test_value = 0
+        pila.push(test_value)
+    
+        self.assertEqual(pila.size(), 5, "Pila tiene un elemento")
+        self.assertIn(test_value, pila.pila, "El valor esta en la pila")
+
+    def test_pull_when_empty(self):
+        pass
+        
+
+    def test_pull_when_not_empty(self):
+        self.assertEqual(1, 1)
+
+    def test_isEmpty_when_empty(self):
+        self.assertEqual(1, 1)
+    
+    def test_isEmpty_when_not_empty(self):
+        self.assertEqual(1, 1)
+
+    def test_size_when_empty(self):
+        self.assertEqual(1, 1)
+
+    def test_size_when_not_empty(self):
+        self.assertEqual(1, 1)
+    
+if __name__ == '__main__':
+    unittest.main()
